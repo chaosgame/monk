0e29d15b437bc0499e250c7b417782fdfee5f3e8
Nathan Lawrence <ch@osga.me>
2011-03-03 18:42:53 -0600

    Added HTML5 local storage support.

--- monk.js
+++ monk.js
@@ -8,43 +8,89 @@ $(document).ready(function() {
 			.show();
 	}
 
-	function createTask() {
-		var task = $("#prototype > li")
+	function createTaskId() {
+		var id;
+		if (localStorage.getItem("task-id") == null)
+			id = 1;
+		else
+			id = 1 + parseInt(localStorage.getItem("task-id"));
+		localStorage.setItem("task-id", id);
+		return "task-" + id;
+	}
+
+
+	function getParent(task) {
+		return task.parents(".task").first();
+	}
+
+	function saveParent(task) {
+		saveTask(getParent(task));
+	}
+
+	function createTask(id) {
+		id = (typeof(id) != "undefined") ? id : createTaskId();
+
+		var task = $("#prototype > .task")
 			.clone(deepWithDataAndEvents=true)
+			.attr("id", id)
 			.draggable({
 				handle: "> .task-title",
 				revert: "invalid",
 				revertDuration: 100,
 			});
-		task.find("#task-add-child,#task-add-next")
+		task.find(".task-add-child,.task-add-next")
 			.droppable({
 				hoverClass: "ui-state-highlight",
 				drop: function(e, ui) {
 					// TODO: There should be a better way to do this
 					$(ui.draggable).css({top:0,left:0});
+
+					var oldParentTask = getParent($(ui.draggable));
 					putTaskNextChild($(ui.draggable), $(this));
+					saveTask(oldParentTask);
+					saveParent($(ui.draggable));
 				}
 			});
 		return task;
 	}
 
+	function saveTask(task) {
+		localStorage.setItem(task.attr("id"), JSON.stringify({
+			title: task.children(".task-title").html(),
+			children: _.map(task.find("> .task-list > .task"), function(x){return $(x).attr("id")})
+		}));
+	}
+
+	function removeTask(task) {
+		localStorage.removeItem(task.attr("id"));
+		// TODO: remove subtasks
+	}
+
 	$("#task-add").keypress(function(e) {
 		if (e.keyCode == 13 && $("#task-add").val().trim() != "") {
-			createTask()
-				.appendTo("#task-root")
-				.find(".task-title")
+			var task = createTask().appendTo("#task-root-list");
+			task.find(".task-title")
 					.html($("#task-add").val())
-				.show();
+			task.show();
+			saveTask(task);
+			saveTask($("#task-root"));
 			$("#task-add").val("");
 		}
 	});
 
 	$(".task-close").click(function(e) {
-		$(e.currentTarget).parent().remove();
+		var task = $(e.currentTarget).closest(".task");
+		var parentTask = getParent(task);
+		removeTask(task);
+		task.remove();
+		saveTask(parentTask);
 	});
 
-	$("#task-add-child,#task-add-next").click(function(e) {
-		putTaskNextChild(createTask(), $(e.currentTarget));
+	$(".task-add-child,.task-add-next").click(function(e) {
+		var task = createTask();
+		putTaskNextChild(task, $(e.currentTarget));
+		saveParent(task);
+		saveTask(task);
 	});
 
 	// TODO: make this work with drag/drop
@@ -54,7 +100,7 @@ $(document).ready(function() {
 	$(".task-title").click(function(e) {
 		var title = $(e.currentTarget);
 		title.hide();
-		title.siblings('.task-title-edit')
+		title.siblings(".task-title-edit")
 			.val(title.html())
 			.show()
 			.focus();
@@ -63,9 +109,27 @@ $(document).ready(function() {
 	$(".task-title-edit").focusout(function(e) {
 		var title = $(e.currentTarget);
 		title.hide();
-		title.siblings('.task-title')
+		title.siblings(".task-title")
 			.html(title.val())
 			.show();
+		saveTask(title.closest(".task"));
 	});
+
+	function loadTask(id) {
+		var task = JSON.parse(localStorage.getItem(id));
+		var element = createTask(id);
+		element.find(".task-title").html(task.title);
+		_.each(task.children, function(child) {
+			loadTask(child).appendTo(element.find("> .task-list"));
+		});
+		return element;
+	}
+
+	var root = JSON.parse(localStorage.getItem("task-root"));
+	if (root != null) {
+		_.each(root.children, function(child) {
+			loadTask(child).appendTo($("#task-root-list"));
+		});
+	}
 });
 
