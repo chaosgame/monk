e82569acf1f9d7e119abf1989bb7bb8c7abb38ff
Nathan Lawrence <ch@osga.me>
2011-03-10 01:00:43 -0600

    Working on new clean API

--- monk.js
+++ monk.js
@@ -1,10 +1,115 @@
 $(document).ready(function() {
-	function putTaskNextChild(task, target) {
-		task.insertAfter(target.closest(".task-list > *"))
-			.show();
+	function Task (element) {
+		if (_.isUndefined(element)) {
+			this.element = Task.create();
+		} else {
+			this.element = element;
+		}
 	}
 
-	function createTaskId() {
+	// TODO: state: moving, creating, none
+	Task.prototype = {
+		insertAfter: function(task) {
+			this.element.insertAfter(task.element.parent(".task-list"));
+			// TODO: update localStorage
+		},
+
+		insertChild: function(task) {
+			this.element.appendTo(task.element.children(".task-list"));
+		},
+
+		remove: function() {
+			function helper(x) {
+				_.each(x.children(), helper);
+				localStorage.removeItem(x.id());
+			}
+
+			var parent = this.parent();
+			helper(this);
+			parent.save();
+		},
+
+		children: function() {
+			// TODO: make simpler
+			return _.map(this.element.find("> .task-list > .task"), function(x) {
+				return new Task($(x));
+			});
+		},
+
+		parent: function() {
+			return new Task(this.element.parents(".task").first());
+		},
+
+		// TODO: simpler attribute mechanism
+		title: function(new_title) {
+			if (_.isUndefined(new_title)) {
+				return this.element.find("> div > .task-title").text();
+			} else {
+				this.element.find("> div > .task-title").text(new_title);
+				this.save();
+			}
+		},
+
+		id: function(new_id) {
+			if (_.isUndefined(new_id)) {
+				return this.element.attr("id");
+			} else {
+				this.element.attr("id", new_id);
+				this.save();
+			}
+		},
+
+		checked: function(new_checked) {
+			if (_.isUndefined(new_checked)) {
+				return this.element.attr("id");
+			} else {
+				this.element.attr("id", new_checked);
+				this.save();
+			}
+		},
+
+		stringify: function() {
+			return JSON.stringify({
+				id: this.id(),
+				title: this.title(),
+				checked: this.checked(),
+				// TODO: Task.prototype.id?
+				children: _.map(this.children(), Task.prototype.id)
+			});
+		},
+
+		isRoot: function() {
+		},
+
+		save: function() {
+			localStorage.setItem(this.id(), this.stringify());
+		}
+	};
+
+	Task.load = function(id) {
+		return Task.initialize(JSON.parse(localStorage.getItem(id)));
+	}
+
+	Task.initialize = function(o) {
+		var task = new Task();
+
+		// TODO: better mechanism for defaults?
+		o.id = _.isUndefined(o.id) ? Task.createId() : o.id;
+		o.title = _.isUndefined(o.title) ? "" : o.title;
+		o.checked = _.isUndefined(o.checked) ? false : o.checked;
+		o.children = _.isUndefined(o.children) ? [] : o.title;
+
+		task.id(o.id);
+		task.title(o.title);
+		task.checked(o.checked);
+
+		_.each(o.children, function(child) {
+			// TODO: Task.load(child).insertChild(task);
+		});
+		return task;
+	}
+
+	Task.createId = function() {
 		var id;
 		if (localStorage.getItem("task-id") == null)
 			id = 1;
@@ -14,75 +119,84 @@ $(document).ready(function() {
 		return "task-" + id;
 	}
 
-
-	function getParent(task) {
-		return task.parents(".task").first();
+	Task.get = function(selector) {
+		return new Task(selector.closest(".task"));
 	}
 
-	function createTask(id) {
-		id = (typeof(id) != "undefined") ? id : createTaskId();
+	Task.insertChildDrop = {
+		hoverClass: "ui-state-highlight",
+		drop : function(e, ui) {
+			var $task = $(ui.draggable);
+			$task.css({top:0,left:0});
 
-		var task = $("#prototype > .task")
+			Task.get($task).insertChild($(this));
+		}
+	};
+
+	Task.insertAfterDrop = {
+		hoverClass: "ui-state-highlight",
+		drop : function(e, ui) {
+			var $task = $(ui.draggable);
+			$task.css({top:0,left:0});
+
+			Task.get($task).insertAfter($(this));
+		}
+	};
+
+	Task.create = function(id) {
+		id = _.isUndefined(id) ? id : createTaskId();
+
+		var $task = $("#prototype > .task")
 			.clone(deepWithDataAndEvents=true)
-			.attr("id", id)
 			.draggable({
 				handle: "> *",
 				revert: "invalid",
 				revertDuration: 100,
 			});
-		task.find(".task-add-child,.task-add-next")
-			.droppable({
-				hoverClass: "ui-state-highlight",
-				drop: function(e, ui) {
-					// TODO: There should be a better way to do this
-					$(ui.draggable).css({top:0,left:0});
 
-					var oldParentTask = getParent($(ui.draggable));
-					putTaskNextChild($(ui.draggable), $(this));
-					saveTask(oldParentTask);
-					saveTask(getParent($(ui.draggable)));
-				}
-			});
-		return task;
+		$task.find(".task-insert-child")
+			.droppable(Task.insertChildDrop);
+
+		$task.find(".task-insert-after")
+			.droppable(Task.insertAfterDrop);
+
+		return $task;
 	}
 
-	function saveTask(task) {
-		localStorage.setItem(task.attr("id"), JSON.stringify({
-			title: task.find("> div > .task-title").text(),
-			children: _.map(task.find("> .task-list > .task"), function(x){return $(x).attr("id")})
-		}));
-	}
-
-	function removeTask(task) {
-		_.each(task.find("> .task-list > .task"), function(child) { removeTask($(child))});
-		localStorage.removeItem(task.attr("id"));
-	}
+	// ******************************************************************
 
 	$("#task-add").keypress(function(e) {
 		if (e.keyCode == 13 && $("#task-add").val().trim() != "") {
-			var task = createTask().appendTo("#task-root-list");
-			task.find("> div > .task-title")
-					.text($("#task-add").val())
-			task.show();
-			saveTask(task);
-			saveTask($("#task-root"));
+			var task = Task.initialize({title: $("#task-add").val()})
+				.insertChild(Task.get($("#task-root")));
 			$("#task-add").val("");
 		}
 	});
 
-	$(".task-close").click(function(e) {
-		var task = $(e.currentTarget).closest(".task");
-		var parentTask = getParent(task);
-		removeTask(task);
-		task.remove();
-		saveTask(parentTask);
+	$(".checkbox-checked,.checkbox-unchecked").click(function(e) {
+		var $checkbox = $(e.currentTarget);
+		if ($checkbox.hasClass("checkbox-unchecked")) {
+			$(e.currentTarget)
+				.removeClass("checkbox-unchecked")
+				.addClass("checkbox-checked");
+		} else if ($checkbox.hasClass("checkbox-checked")) {
+			$(e.currentTarget)
+				.removeClass("checkbox-checked")
+				.addClass("checkbox-unchecked");
+		}
 	});
 
-	$(".task-add-child,.task-add-next").click(function(e) {
-		var task = createTask();
-		putTaskNextChild(task, $(e.currentTarget));
-		saveTask(getParent(task));
-		saveTask(task);
+	$(".delete").click(function(e) {
+		var task = Task.get($(e.currentTarget));
+		task.remove();
+	});
+
+	$(".task-insert-child").click(function(e) {
+		new Task().insertChild(Task.get($(e.currentTarget)));
+	});
+
+	$(".task-insert-after").click(function(e) {
+		new Task().insertAfter(Task.get($(e.currentTarget)));
 	});
 
 	// TODO: make this work with drag/drop
@@ -100,16 +214,19 @@ $(document).ready(function() {
 	});
 
 	$(".task-title-edit").focusout(function(e) {
-		var editing = $(e.currentTarget);
-		if (editing.hasClass(".task-title-moving"))
+		var $editing = $(e.currentTarget);
+		var $title = $editing.siblings(".task-title");
+
+		// TODO: keep the state of the item, if it's currently being created
+		// destroy it if it's empty
+		if ($editing.hasClass("task-title-moving"))
 			return;
-		var title = editing.siblings(".task-title");
-		if (editing.val().trim() != "") {
-				title.text(editing.val());
-				saveTask(title.closest(".task"));
+		if ($editing.val().trim() != "") {
+			$title.text($editing.val());
+			Task.get($title).save();
 		}
-		editing.hide();
-		title.show();
+		$editing.hide();
+		$title.show();
 	});
 
 	var shiftDown = false;
@@ -117,37 +234,37 @@ $(document).ready(function() {
 	$(".task-title-edit").keydown(function(e) {
 		switch (e.keyCode) {
 		case 9:
-			var title = $(e.currentTarget);
-			var task = title.closest(".task");
+			var $title = $(e.currentTarget);
+			var task = Task.get($title);
 
 			if (shiftDown) {
-				var parentTask = getParent(task);
+				var parent = task.parent();
 
-				if (!getParent(parentTask).size())
+				if (!parent.isRoot())
 					return false;
 
-				title.addClass(".task-title-moving");
+				$title.addClass("task-title-moving");
 
-				task.insertAfter(parentTask);
-				saveTask(parentTask);
-				saveTask(getParent(task));
+				task.insertAfter(parent);
+				parent.save();
+				task.parent().save();
 
-				title.removeClass(".task-title-moving");
-				title.focus().select();
+				$title.removeClass("task-title-moving");
+				$title.focus().select();
 			} else {
-				var prev = task.prev(".task");
+				var prev = task.prev();
 
 				if (!prev.size())
 					return false;
 
-				title.addClass(".task-title-moving");
+				$title.addClass("task-title-moving");
 
-				task.appendTo(prev.children(".task-list"));
-				saveTask(prev);
-				saveTask(getParent(prev));
+				task.insertAfter(prev.find("> div > .task-list"));
+				prev.save();
+				getParent(prev).save();
 
-				title.removeClass(".task-title-moving");
-				title.focus().select();
+				$title.removeClass("task-title-moving");
+				$title.focus().select();
 			}
 			return false;
 		case 16:
@@ -168,56 +285,11 @@ $(document).ready(function() {
 		}
 	});
 
-	function loadTask(id) {
-		var task = JSON.parse(localStorage.getItem(id));
-		var element = createTask(id);
-		element.find("> div > .task-title").text(task.title);
-		_.each(task.children, function(child) {
-			loadTask(child).appendTo(element.find("> .task-list"));
-		});
-		return element;
-	}
-
 	var root = JSON.parse(localStorage.getItem("task-root"));
 	if (root != null) {
 		_.each(root.children, function(child) {
-			loadTask(child).appendTo($("#task-root-list"));
+			Task.get($("#task-root")).insertChild(Task.load(child));
 		});
 	}
-
-	$("#task-root-list > .task-add-child")
-		.droppable({
-			hoverClass: "ui-state-highlight",
-			drop: function(e, ui) {
-				// TODO: There should be a better way to do this
-				$(ui.draggable).css({top:0,left:0});
-
-				var oldParentTask = getParent($(ui.draggable));
-				putTaskNextChild($(ui.draggable), $(this));
-				saveTask(oldParentTask);
-				saveTask(getParent($(ui.draggable)));
-			}
-		});
-
-	$(".checkbox-checked,.checkbox-unchecked").click(function(e) {
-		var $checkbox = $(e.currentTarget);
-		if ($checkbox.hasClass("checkbox-unchecked")) {
-			$(e.currentTarget)
-				.removeClass("checkbox-unchecked")
-				.addClass("checkbox-checked");
-		} else if ($checkbox.hasClass("checkbox-checked")) {
-			$(e.currentTarget)
-				.removeClass("checkbox-checked")
-				.addClass("checkbox-unchecked");
-		}
-	});
-
-	$(".delete").click(function(e) {
-		var task = $(e.currentTarget).closest(".task");
-		var parentTask = getParent(task);
-		removeTask(task);
-		task.remove();
-		saveTask(parentTask);
-	});
 });
 
